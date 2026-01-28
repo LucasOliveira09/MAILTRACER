e05e2d44c6066c7bbf8781f2b7aaf335181c5189
LucasOliveira09 <lucasleiteoliveira09@gmail.com>
2026-01-28 18:34:42 -0300

    da

--- app.py
+++ app.py
@@ -30,5 +30,7 @@ def analisar():
         print(f"Erro no servidor: {e}")
         return jsonify({'erro': f'Erro interno: {str(e)}'}), 500
 
+
+
 if __name__ == '__main__':
     app.run(debug=True)

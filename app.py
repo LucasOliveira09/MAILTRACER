162feaf4eb12ad3fe87791d784e4e9bc64837c72
LucasOliveira09 <lucasleiteoliveira09@gmail.com>
2026-01-28 00:50:20 -0300

    Atualização para lançar front

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/analisar', methods=['POST'])
 def analisar():
     try:
